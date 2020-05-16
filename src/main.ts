f44754ed66659fade785fb4e39818051171aa4a7
anupbista <bistaanup77@gmail.com>
2020-05-16 20:30:39 +0545

    CORS added

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const port = process.env.PORT || 8080;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors();
 	app.setGlobalPrefix('api/v1');
 
 	const options = new DocumentBuilder()
