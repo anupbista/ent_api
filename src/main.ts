7e4c173831f4e4a53a7c0179acddc4afc6d1c8cd
anupbista <bistaanup77@gmail.com>
2020-05-17 22:51:06 +0545

    Static assets added

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 const port = process.env.PORT || 8080;
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	const cors_options = {
         "origin": "*",
         "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
         "preflightContinue": false,
         "optionsSuccessStatus": 204,
 		"credentials":true,
-		"allowedHeaders": "Content-Type, Accept, Origin, Authorization",
+		"allowedHeaders": "Content-Type, Accept, Origin, Authorization, X-Requested-With",
     }
 	app.enableCors(cors_options);
 	app.setGlobalPrefix('api/v1');
-
+	app.useStaticAssets(join(__dirname, '..', 'uploads'));
 	const options = new DocumentBuilder()
 		.setTitle('Ent API')
 		.setDescription('API for Ent')
